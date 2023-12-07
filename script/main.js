434fc69833cf844761ca35eca0e06e81eb27c54a
Sachiiinn6249 <sachiiinn6249@gmail.com>
2023-12-07 19:30:46 +0530

    update

---
+++ script/main.js
@@ -0,0 +1,25 @@
+const menuIcon = document.querySelector("#menu-icon");    
+const navbar = document.querySelector(".navbar");
+const navbg = document.querySelector(".nav-bg");
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+  navbg.classList.toggle("active");
+});
+
+var image = document.getElementsByClassName('thumbnail');
+new simpleParallax(image, {
+	overflow: true
+});
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
